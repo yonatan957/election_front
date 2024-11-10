055d433e53e9ca4dcacc08247d96b6dcab0cd80a
yonatan957 <yonatan22660@gmail.com>
2024-11-10 17:16:34 +0200

    create routing and components

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/Nav'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Vote from './components/pages/Vote'
+import Statistics from './components/pages/Statistics'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div>
+    <Nav/>
+    <Routes>
+      <Route path='login' element={<Login/>}/>
+      <Route path='register' element={<Register/>}/>
+      <Route path='vote' element={<Vote/>}/>
+      <Route path='Statistics' element={<Statistics/>}/>
+    </Routes>
+  </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
